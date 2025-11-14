fea36bb78d178a818eefb7a4088bc5af9039b122
H - Komila <komilaxibziddinova06@gmail.com>
2025-11-15 00:31:53 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function addTodo(){
+    const input = document.getElementById("input");
+    const text = input.value.trim("");
+
+    if(text === "") return;
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span> ${text} </span>
+     <button onclick = "deleteTodo(this)">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    `;
+    
+
+    const ul = document.getElementById("userList");
+    ul.appendChild(li);
+    input.value = "";
+
+
+}
+
+function deleteTodo(button){
+    const li = button.parentElement;
+    li.remove();
+}
